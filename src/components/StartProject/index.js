38fb149a5654f8e2119ec49069a132c6c673e6e0
Jacob Chademwiri <jchademwiri@gmail.com>
2021-12-31 10:43:38 +0200

    CTA fixed

--- src/components/StartProject/index.js
+++ src/components/StartProject/index.js
@@ -5,28 +5,19 @@ import Image from 'next/image';
 const StartProject = () => {
 	return (
 		<section className={styles.container}>
-			<div className={styles.container__text}>
-				<h2>Have a Project on mind?</h2>
-				<p>
-					Whether you have a small business that needs a web presence or a more
-					customized ecommerce shopping cart solution, our specialized team will
-					collaborate with you to develop a strategic solution to strengthen
-					your brand.
-				</p>
-				<Link className={styles.project_link} href='#'>
-					Let&apos;s get started
-				</Link>
-			</div>
-			<div className={styles.container__image}>
-				{/* <Image
-					className={styles.container__image__image}
-					placeholder='blur'
-					blurDataURL='/images/start-a-project.jpg'
-					src='/images/start-a-project.jpg'
-					width={800}
-					height={574}
-					alt='Start a project'
-				/> */}
+			<div className={styles.container__content}>
+				<div className={styles.container__content__text}>
+					<h2>Have a Project on mind?</h2>
+					<p>
+						Whether you have a small business that needs a web presence or a
+						more customized ecommerce shopping cart solution, our specialized
+						team will collaborate with you to develop a strategic solution to
+						strengthen your brand.
+					</p>
+					<Link className={styles.project_link} href='#'>
+						Let&apos;s get started
+					</Link>
+				</div>
 			</div>
 		</section>
 	);
