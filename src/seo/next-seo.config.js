f5b818e281910a07800dd29178dc9ee3ef529042
Jacob Chademwiri <jchademwiri@gmail.com>
2022-01-07 15:28:20 +0200

    SEO

---
+++ src/seo/next-seo.config.js
@@ -0,0 +1,34 @@
+export default {
+	title: 'Jacob Chademwiri | Web Developer',
+	description: 'A professional web developer and digital marketing specialist',
+	canonical: 'https://www.playhousemedia.net/',
+	openGraph: {
+		url: 'https://www.playhousemedia.net/',
+		title: 'Jacob Chademwiri | Web Developer',
+		description: 'A professional web developer and digital marketing specialist',
+		images: [
+			{
+				url: 'https://www.example.ie/og-image-01.jpg',
+				width: 800,
+				height: 600,
+				alt: 'Og Image Alt',
+				type: 'image/jpeg'
+			},
+			{
+				url: 'https://www.example.ie/og-image-02.jpg',
+				width: 900,
+				height: 800,
+				alt: 'Og Image Alt Second',
+				type: 'image/jpeg'
+			},
+			{ url: 'https://www.example.ie/og-image-03.jpg' },
+			{ url: 'https://www.example.ie/og-image-04.jpg' }
+		],
+		site_name: 'Jacob Chademwiri | Web Developer'
+	},
+	twitter = {
+		handle: '@jchademwiri',
+		site: '@jchademwiri',
+		cardType: 'summary_large_image'
+	},
+};
