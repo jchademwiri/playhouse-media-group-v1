329948288dfb781cc5e27d6825bcefe434b12da8
Jacob Chademwiri <jchademwiri@gmail.com>
2022-01-08 16:28:05 +0200

    added what we do section

--- src/components/WhatWeDo/index.js
+++ src/components/WhatWeDo/index.js
@@ -1,4 +1,5 @@
 import styles from './WhatWeDo.module.scss';
+import Image from 'next/image';
 
 const WhatWeDo = () => {
 	return (
@@ -15,20 +16,70 @@ const WhatWeDo = () => {
 					{/* </div> */}
 					<div className={styles.boxes}>
 						<div className={styles.box}>
-							<div className={styles.icon}>Icon Placeholder</div>
+							<div className={styles.icon}>
+								<Image
+									src={'/images/analysis.svg'}
+									width={100}
+									height={100}
+									alt='Analysis'
+									blurDataURL='/images/analysis.svg'
+									placeholder='blur'
+								/>
+							</div>
 							<h4>Analysis</h4>
 						</div>
 						<div className={styles.box}>
-							<div className={styles.icon}>Icon Placeholder</div>
+							<div className={styles.icon}>
+								<Image
+									src={'/images/design.svg'}
+									width={100}
+									height={100}
+									alt='Design'
+									blurDataURL='/images/design.svg'
+									placeholder='blur'
+								/>
+							</div>
 							<h4>Design</h4>
 						</div>
 						<div className={styles.box}>
-							<div className={styles.icon}>Icon Placeholder</div>
+							<div className={styles.icon}>
+								<Image
+									src={'/images/development-01.svg'}
+									width={100}
+									height={100}
+									alt='Development'
+									blurDataURL='/images/development-01.svg'
+									placeholder='blur'
+								/>
+							</div>
 							<h4>Development</h4>
 						</div>
 						<div className={styles.box}>
-							<div className={styles.icon}>Icon Placeholder</div>
-							<h4>Testing & Deploy</h4>
+							{/* <div className={styles.icon}> */}
+							<Image
+								src={'/images/testing.svg'}
+								width={100}
+								height={100}
+								alt='Testing'
+								blurDataURL='/images/testing.svg'
+								placeholder='blur'
+							/>
+							<h4>Testing</h4>
+							{/* </div> */}
+						</div>
+						<div className={styles.box}>
+							<div className={styles.icon}>
+								<Image
+									src={'/images/deployment.svg'}
+									width={100}
+									height={100}
+									alt='Deployment'
+									blurDataURL='/images/deployment.svg'
+									placeholder='blur'
+								/>
+							</div>
+
+							<h4>Deployment</h4>
 						</div>
 					</div>
 				</div>
