c46ce7b9ed0c216468888d1780308f5f66112bb6
Jacob Chademwiri <jchademwiri@gmail.com>
2022-04-16 13:15:20 +0200

    local width changes

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,14 +5,15 @@ const Hero = () => {
 		<header className={styles.container}>
 			<div className={styles.container__hero}>
 				<div className={styles.container__left}>
-					<h3 className={styles.title}>Welcome, I&apos;m Jacob</h3>
+					<h3 className={styles.title}>Welcome, to our website</h3>
 					<h1 className={styles.hero_text}>
 						I design and build <br />
 						<span className={styles.custom_web}>custom web solutions</span>
 						<br /> for your Brand.
 					</h1>
 					<p>
-						I love to visualise ideas and make them come alive in a website.
+						We know how to get results. We'll take you through the exact same
+						process we've used to generate over 20 000 leads a month.
 					</p>
 					<div className={styles.buttons}>
 						<div className={styles.btn}>
