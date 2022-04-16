c74dfb9ea402cd9bc65826810258117d056cddba
Jacob Chademwiri <jchademwiri@gmail.com>
2022-04-16 14:11:54 +0200

    made changes for the first live test preparation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,30 +5,19 @@ const Hero = () => {
 		<header className={styles.container}>
 			<div className={styles.container__hero}>
 				<div className={styles.container__left}>
-<<<<<<< HEAD
-					<h3 className={styles.title}>Welcome, to our website</h3>
-=======
 					{/* <h3 className={styles.title}>
 						Welcome To the Home of Creative Design
 					</h3> */}
->>>>>>> c355305a6509be49846910477ed1df52d264cdcd
 					<h1 className={styles.hero_text}>
 						Welcome To <br />
 						<span className={styles.custom_web}>Playhouse Media Group</span>
 						<br />
 						Your Digital Agency.
 					</h1>
-<<<<<<< HEAD
-					<p>
-						We know how to get results. We'll take you through the exact same
-						process we've used to generate over 20 000 leads a month.
-					</p>
-=======
 					<small className={styles.quote}>
 						{/* We love to visualise ideas and make them come alive in a website. */}
 						{/* Home Of Creative Designs */}
 					</small>
->>>>>>> c355305a6509be49846910477ed1df52d264cdcd
 					<div className={styles.buttons}>
 						<div className={styles.btn}>
 							<a
