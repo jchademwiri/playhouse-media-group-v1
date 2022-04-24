1e39b473ca7360afd18bec0ab087e812575ab0c8
Jacob Chademwiri <jchademwiri@gmail.com>
2022-04-24 04:39:38 +0200

    added a sitemap-removed server

--- next-sitemap.js
+++ next-sitemap.js
@@ -6,8 +6,8 @@ module.exports = {
 	robotsTxtOptions: {
 		policies: [{ userAgent: '*', allow: '/' }],
 		additionalSitemaps: [
-			`${siteUrl}/sitemap-0.xml`,
-			// `${siteUrl}/sitemap.xml`,
+			// `${siteUrl}/sitemap-0.xml`,
+			`${siteUrl}/sitemap.xml`,
 			// `${siteUrl}/server-sitemap.xml`,
 		],
 	},
