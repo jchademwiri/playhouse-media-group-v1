50d0661cc18ed319370145096577f213986c97d1
Jacob Chademwiri <jchademwiri@gmail.com>
2022-07-27 07:16:27 +0200

    blog post redesgn and remeved commented code

--- src/pages/[slug]/index.js
+++ src/pages/[slug]/index.js
@@ -70,11 +70,9 @@ const BlogPost = ({ post }) => {
 			<NextSeo {...SEO} />
 			<ScrollIndicator />
 
-			{/* <div className=" bg-[url('/images/bg.jpg')] bg-no-repeat  min-h-[400px] shadow-[inset_0_0_1000px_rgb(27,25,52,0.8)]"> */}
 			<div className='  min-h-[400px] shadow-[inset_0_0_1000px_rgb(27,25,52,0.8)]'>
 				<div className='h-[90px]'></div>
-				{/* <div className="bg-[url('/images/bg.jpg')]  min-h-[400px] shadow-[inset_0_0_1000px_rgb(27,25,52,0.8)]"> */}
-				{/* <div className=' min-h-[400px] shadow-[inset_0_0_1000px_rgb(27,25,52,0.8)]'> */}
+
 				<div className=' min-h-[400px]'>
 					<div className='sm:w-11/12 lg:w-7/12 mx-auto rounded-t-lg  bg-[#1b1934]/80'>
 						<Image
@@ -161,85 +159,6 @@ const BlogPost = ({ post }) => {
 					</div>
 				</div>
 			</div>
-
-			{/* <div className='relative w-full h-[600px] overflow-hidden'>
-				<div className='absolute top-0 left-0 z-10 w-full h-full bg-body opacity-60'></div>
-				<div className='grid'>
-					<div className='absolute z-30  grid w-[500px] h text-center '>
-						<h1 className='text-5xl font-bold'>{post.title}</h1>
-						<h2 className='text-xl '>{post.exempt}</h2>
-					</div>
-				</div>
-
-				<Image
-					className='object-cover '
-					src={urlFor(post.mainImage).url()}
-					width={1920}
-					height={1080}
-					alt={post.title}
-					placeholder='blur'
-					blurDataURL={urlFor(post.mainImage).url()}
-					objectFit='cover'
-				/>
-			</div> */}
-
-			{/* <section className={styles.container}>
-				<h1 className={styles.title}>{post.title}</h1>
-				<div className={styles.meta}>
-					<div className={styles.author}>
-						<Image
-							className={styles.author_image}
-							src={urlFor(post.author.image).url()}
-							width={50}
-							height={50}
-							alt={post.author.name}
-							placeholder='blur'
-							blurDataURL={urlFor(post.author.image).url()}
-							objectFit='cover'
-						/>
-						<div className={styles.author_details}>
-							<small className={styles.author_name}>{post.author.name}</small>
-							<small>
-								Published: {moment(post.publishedAt).format('DD MMMM YYYY')}
-							</small>
-						</div>
-					</div>
-
-					<div className={styles.categories}>
-						<div>
-							{post.categories &&
-								post.categories.map((category) => (
-									<span key={category.slug}>{category.title}</span>
-								))}
-						</div>
-					</div>
-				</div>
-
-				<article className={styles.content}>
-					<h1 className={styles.title}>{post.title}</h1>
-					<div>
-						<BlockContent blocks={post.body} serializers={serializers} />
-					</div>
-					<small>
-						Latst Updated: {moment(post._updatedAt).format('DD MMMM YYYY')}
-					</small>
-				</article>
-				<hr />
-
-				<div className={styles.postLinks}>
-					<div>
-						<Link href={`/#`}>
-							<a className={styles.postLink}>Previous Post</a>
-						</Link>
-					</div>
-
-					<div>
-						<Link href={`/#`}>
-							<a className={styles.postLink}>Next Post</a>
-						</Link>
-					</div>
-				</div>
-			</section> */}
 		</>
 	);
 };
